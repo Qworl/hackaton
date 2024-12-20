0da03abc41565b277057aec1df49d579d792168e
Grigory <i@kirsanovgr.ru>
2024-12-20 22:46:30 +0300

    BF supp

--- django/myapp/forms.py
+++ django/myapp/forms.py
@@ -4,4 +4,4 @@ from .models import Record
 class RecordForm(forms.ModelForm):
     class Meta:
         model = Record
-        fields = ['id', 'name', 'title', 'content', 'classification', 'is_finance', 'tag']
+        fields = ['id', 'name', 'title', 'content']
